5c2a1e256c629315169ffc63451eb77bf5985c95
Albert <albertrichards3@gmail.com>
2021-06-18 14:32:37 +0000

    added the bare bones

---
+++ application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']=getenv('URI_database_project')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.config['SECRET_KEY']=getenv('S_key')
+
+db = SQLAlchemy(app)
+
+from application import routes
---
+++ application/models.py
@@ -0,0 +1,20 @@
+from application import db
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    surname = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    date_of_birth = db.Column(db.Date, nullable=False)
+
+class Exercises(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    exercise_name = db.Column(db.String(20), nullable=False, unique=True)
+
+class User_stats(db.Model):
+    entry_no = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
+    personal_best = db.Column(db.Decimal(3,1), nullable=False, default=0.0)
+ 
