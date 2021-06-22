9b26556e1aa5a25908071f2aaa23e05b58b2e0db
Albert <albertrichards3@gmail.com>
2021-06-22 16:57:42 +0000

    added more to forms

--- application/forms.py
+++ application/forms.py
@@ -1,10 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import ""
+from wtforms import StringField, DateField, SelectField, SubmitField, DecimalField
+from wtforms.validators import DataRequired, Length
 
 class RegisterForm(FlaskForm):
-    username=StringField('Username:')
-    first_name = StringField('First Name:')
-    surname = StringField('Surname:')
-    
+    username=StringField('Username', validators=[
+        DataRequired(),
+        Length(min=2,max=20)
+    ])
+    first_name = StringField('First Name')
+    surname = StringField('Surname')
+    email = StringField('Email')
+    date_of_birth = DateField('Date of Birth')
+    submit = SubmitField('Submit')
 
+class ExerciseForm(FlaskForm):
+    exercise_name=StringField('Name of exercise')
+    submit = SubmitField('Submit')
+
+class RecordForm(FlaskForm):
+    exercise_name=SelectField('Exercise' choices=exercise_list)
+    personal_best=DecimalField('Enter your personal best', places=1)
+    submit = SubmitField('Submit')
